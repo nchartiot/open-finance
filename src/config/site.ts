b172972b19a5c03e6e434cb8c0f8dde09ffb60c7
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-08-08 18:30:01 -0300

    Initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Green Edge - Starter',
+  url: 'https://github.com/nchartiot/green-edge-starter',
+  ogImage: '',
+  description: 'A starter project for NextJS 13 apps using supabase, lemonsqueezy and shadcn-ui',
+  links: {
+    github: 'https://github.com/nchartiot/green-edge-starter',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
